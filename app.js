d47727e35e9ecf8b4cec07d550a74c2818557752
trimming <lexgolikoff@gmail.com>
2022-12-17 14:54:08 +0300

    copied the catalog page from the course prof layout

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 'use strict';
 const goods = [
-    { image: "img/card1.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card2.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/card3.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/card4.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card5.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card6.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card7.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card8.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-    { image: "img/catalog_card9.png", title: "ELLERY X M'O CAPSULE", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
-
+    { image: "images/card_1.png", title: "Product 01", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$52.00" },
+    { image: "images/card_7.jpg", title: "Product 02", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$65.00" },
+    { image: "images/card_3.png", title: "Product 03", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$47.50" },
+    { image: "images/card_4.png", title: "Product 04", text: "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", price: "$82.00" },
 ];
 
-const $goodList = document.querySelector('.cards_block');
+const $goodList = document.querySelector('.filter__cards');
 
 const renderGoodsItem = ({ image, title, text, price }) => {
-    return `<div class="card">
-    <img src=${image} alt="foto">
-    <h5 class="card_heading">${title}</h5>
-    <p class="card_text">${text}</p>
-    <h6 class="card_price">${price}</h6>
+    return `<div class="b-card">
+    <div class="b-card__wrap">
+        <img class="b-card__img" src="images/card_10.jpg" alt="model">
+        <div class="b-card__wrapBtn">
+            <button class="b-card__button" data-type="Product 07"><img src="images/cart.svg"
+                    data-type="Product 07" alt="cart">Add
+                to
+                Cart</button>
+        </div>
+    </div>
+    <div class="b-card__info">
+        <h4 class="b-card__infoItem">Product 07</h4>
+        <p class="b-card__infoText">Known for her sculptural takes on traditional tailoring,
+            Australian arbiter of cool Kym Ellery teams up with Moda Operandi.</p>
+        <p class="b-card__infoPrice">$<span class="b-card__price">112.00</span></p>
+    </div>
 </div>`;
 };
 
