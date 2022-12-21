fb70c05012e508ba8a35f476adecf953b3eeffe5
lexgo <lexgolikoff@gmail.com>
2022-12-22 00:30:15 +0300

    started third task from lesson 2

---
+++ humburger/script.js
@@ -0,0 +1,28 @@
+'use strict';
+class Hamburger {
+    constructor(size, stuffing) {
+        this._size = size;
+        this._stuffing = stuffing;
+    }
+    addTopping(topping) {
+
+    } // Добавить добавку }
+    removeTopping(topping) {
+
+    } // Убрать добавку }
+    getToppings(topping) {
+
+    } // Получить список добавок }
+    getSize() {
+        return this._size;
+    } // Узнать размер гамбургера }
+    getStuffing() {
+        return this._stuffing;
+    } // Узнать начинку гамбургера }
+    calculatePrice() {
+
+    } // Узнать цену }
+    calculateCalories() {
+
+    } // Узнать калорийность }
+}
