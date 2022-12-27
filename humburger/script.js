b9d27e421faba1fba622b39568b41b429ea411b8
trimming <lexgolikoff@gmail.com>
2022-12-27 16:58:34 +0300

    some update from script.js

--- humburger/script.js
+++ humburger/script.js
@@ -1,6 +1,6 @@
 'use strict';
 class Hamburger {
-    constructor(size, stuffing) {
+    constructor(size, stuffing, topping) {
         this._size = size;
         this._stuffing = stuffing;
     }
@@ -14,44 +14,57 @@ class Hamburger {
 
     } // Получить список добавок }
     getSize() {
-        return this._size;
+        return this._size._name;
     } // Узнать размер гамбургера }
     getStuffing() {
-        return this._stuffing;
+        return this._stuffing._name;
     } // Узнать начинку гамбургера }
     calculatePrice() {
-
+        return this._size._price + this._stuffing._price;
     } // Узнать цену }
     calculateCalories() {
-
+        return this._size._calories + this._stuffing._calories;
     } // Узнать калорийность }
 }
 
 class Size {
-    constructor(price, calories) {
+    constructor({ price, calories, name }) {
         this._price = price;
         this._calories = calories;
+        this._name = name;
+    }
+}
+class Stuffing {
+    constructor({ price, calories, name }) {
+        this._price = price;
+        this._calories = calories;
+        this._name = name;
     }
 }
 let small = {
     price: 50,
     calories: 20,
+    name: 'Маленький',
 }
 let big = {
     price: 100,
     calories: 40,
+    name: 'Большой',
 }
 let withCheese = {
     price: 10,
     calories: 20,
+    name: 'с сыром',
 }
 let withSalad = {
     price: 20,
     calories: 5,
+    name: 'с салатом',
 }
 let withPotato = {
     price: 15,
     calories: 10,
+    name: 'с картошкой',
 }
 let paprika = {
     price: 15,
@@ -61,4 +74,12 @@ let mayonnaise = {
     price: 20,
     calories: 5,
 }
-userHumburger = 
+let sizeHumburger = new Size(big);
+console.log(sizeHumburger);
+let stuffingHumburger = new Stuffing(withPotato);
+console.log(stuffingHumburger);
+let userHumburger = new Hamburger(sizeHumburger, stuffingHumburger);
+console.log(userHumburger);
+console.log(userHumburger.calculatePrice());
+console.log(userHumburger.calculateCalories());
+console.log(`${userHumburger.getSize()} ${userHumburger.getStuffing()}`);
