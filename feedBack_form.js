1bc16684959a7e1acfb2983a32cbcce63d2ee823
lexgo <lexgolikoff@gmail.com>
2023-01-30 21:40:04 +0300

    add check to form feedback

--- feedBack_form.js
+++ feedBack_form.js
@@ -3,25 +3,6 @@
 //Элемент кнопки отправки формы.
 const buttonFeedbackEl = document.querySelector('.b-feedbackBlock__button');
 
-let name = {
-    element: document.getElementById('subscribeName'),
-    regex: /^([a-zA-Z\s?]+)$/mi,
-    message: '<span class = "subscribe" id = "nameMessage" style = "color:#E05C6E">Имя должно содержать только буквы!</span>',
-    id: "nameMessage"
-}
-let phone = {
-    element: document.getElementById('subscribePhone'),
-    regex: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
-    message: '<span class = "subscribe" id = "phoneMessage" style = "color:#E05C6E">Телефон должен быть формата +7(000)000-0000!</span>',
-    id: "phoneMessage"
-}
-let email = {
-    element: document.getElementById('subscribeEmail'),
-    regex: /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/,
-    message: '<span class = "subscribe" id = "emailMessage" style = "color:#E05C6E">Почта введена не корректно!</span>',
-    id: "emailMessage"
-}
-
 class UserInput {
     constructor({ element, regex, message, id }) {
         this.element = element;
@@ -30,12 +11,14 @@ class UserInput {
         this.id = id;
     }
     testInput() {
-        console.log(this.element);
-        console.log(this.regex);
         if (!(this.regex.test(this.element.value))) {
-            this.element.style.border = 'solid 3px #E05C6E';
-            this.element.insertAdjacentHTML('afterend', `${this.message}`);
-            this.element.style.background = '#ffd9d9'
+            if (document.querySelector(`.subscribe[id = "${this.id}"]`)) {
+
+            } else {
+                this.element.style.border = 'solid 3px #E05C6E';
+                this.element.insertAdjacentHTML('afterend', `${this.message}`);
+                this.element.style.background = '#ffd9d9'
+            }
         } else if (document.querySelector(`.subscribe[id = "${this.id}"]`)) {
             const messageEl = document.querySelector(`.subscribe[id = "${this.id}"]`);
             messageEl.style.display = 'none';
@@ -48,8 +31,39 @@ class UserInput {
     }
 }
 
-const form = new UserInput(email);
+class InputsList {
+    constructor(inputs) {
+        this.inputs = inputs;
+    }
+    addInputToForm() {
+        this.inputs.forEach(input => {
+            input.testInput();
+        });
+    }
+}
+
+const inputs = [
+    new UserInput({
+        element: document.getElementById('subscribeName'),
+        regex: /^([a-zA-Z\s?]+)$/mi,
+        message: '<span class = "subscribe" id = "nameMessage" style = "color:#E05C6E">Имя должно содержать только буквы!</span>',
+        id: "nameMessage"
+    }),
+    new UserInput({
+        element: document.getElementById('subscribePhone'),
+        regex: /^\+7\(\d{3}\)\d{3}-\d{4}$/,
+        message: '<span class = "subscribe" id = "phoneMessage" style = "color:#E05C6E">Телефон должен быть формата +7(000)000-0000!</span>',
+        id: "phoneMessage"
+    }),
+    new UserInput({
+        element: document.getElementById('subscribeEmail'),
+        regex: /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/,
+        message: '<span class = "subscribe" id = "emailMessage" style = "color:#E05C6E">Почта введена не корректно!</span>',
+        id: "emailMessage"
+    })];
+
+const form = new InputsList(inputs);
 buttonFeedbackEl.addEventListener('click', () => {
-    form.testInput();
+    form.addInputToForm();
 });
 
