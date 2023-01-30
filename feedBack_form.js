03a6d98b9496daadf154653726c7cbd8c26c5ed9
lexgo <lexgolikoff@gmail.com>
2023-01-30 08:32:04 +0300

    some update to feedback_form.js

--- feedBack_form.js
+++ feedBack_form.js
@@ -8,16 +8,19 @@ const inputEmailEl = document.getElementById('subscribeEmail');
 //Элемент кнопки отправки формы.
 const buttonFeedbackEl = document.querySelector('.b-feedbackBlock__button');
 
-let regExpName = new RegExp('[a-zA-z]+', 'i');
-let regExpPhone = new RegExp('\+7\(\d{3}\)\d{3}-\d{4}');
+let regExpName = new RegExp('^(\\s?[a-zA-z]+\\s?)+$', 'gmi');
+let regExpPhone = new RegExp('\\+7\\(\\d{3}\\)\\d{3}-\\d{4}');
 let regExpEmail = new RegExp('^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$');
 
+console.log(inputNameEl);
+buttonFeedbackEl.addEventListener('click', () => {
+    console.log(inputNameEl.value);
+    if (!(regExpName.test(inputNameEl.value))) {
+        inputNameEl.style.border = 'solid 3px #E05C6E';
+        inputNameEl.insertAdjacentHTML('afterend', '<span style = "color:#E05C6E">Имя должно содержать только буквы!</span>')
+    } else {
 
-buttonFeedbackEl.addEventListener('click', validForm);
-
-function validForm(input, regex) {
-    if (!(regex.test(input.textContent))) {
-        input.style.border = '#E05C6E';
     }
-}
-validForm(inputNameEl, regExpName);
+});
+
+
