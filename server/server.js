90a3c7a0e68bd4e7f276c2627140ac2e9aa9bee4
Alexey Golikov <lexgolikoff@gmail.com>
2023-03-15 19:13:57 +0300

    builded of project

--- server/server.js
+++ server/server.js
@@ -4,39 +4,40 @@ const fs = require('fs');
 
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static('./dist'));
 app.use(bodyParser.json());
 
-app.get('/catalogData', (req, res) => {
+app.get('/api/catalog', (req, res) => {
     fs.readFile('./server/data/catalog.json', 'utf8', (err, data) => {
         res.send(data);
+        
     });
 });
 
-app.get('/addToCart', (req, res) => {
+app.get('/api/cart', (req, res) => {
     fs.readFile('./server/data/cart.json', 'utf8', (err, data) => {
         res.send(data);
     });
 });
 
-app.get('/removeFromCart', (req, res) => {
-    fs.readFile('./data/cart.json', 'utf8', (err, data) => {
+app.get('/api/removeFromCart', (req, res) => {
+    fs.readFile('./server/data/cart.json', 'utf8', (err, data) => {
         res.send(data);
     });
 });
 
-app.get('/stats', (req, res) => {
-    fs.readFile('./data/stats.json', 'utf8', (err, data) => {
+app.get('/api/stats', (req, res) => {
+    fs.readFile('./server/data/stats.json', 'utf8', (err, data) => {
         res.send(data);
     });
 });
 
 
-app.post('/addToCart', (req, res) => {
+app.post('/api/cart', (req, res) => {
     fs.readFile('./server/data/cart.json', 'utf8', (err, data) => {
         const cart = JSON.parse(data);
         cart.push(req.body);
-
+        console.log(req.body);
         fs.writeFile('./server/data/cart.json', JSON.stringify(cart), (err) => {
             console.log('done');
             res.end();
@@ -44,27 +45,24 @@ app.post('/addToCart', (req, res) => {
     });
 });
 
-app.post('/removeFromCart', (req, res) => {
-    fs.readFile('./data/cart.json', 'utf8', (err, data) => {
+app.post('/api/removeFromCart', (req, res) => {
+    fs.readFile('./server/data/cart.json', 'utf8', (err, data) => {
         const cart = JSON.parse(data);
         const list = cart.filter(good =>
             good.id_product !== req.body.id_product);
 
-        fs.writeFile('./data/cart.json', JSON.stringify(list), (err) => {
+        fs.writeFile('./server/data/cart.json', JSON.stringify(list), (err) => {
             console.log('done');
             res.end();
         });
     });
 });
 
-app.post('/stats', (req, res) => {
-    fs.readFile('./data/stats.json', 'utf8', (err, data) => {
-        const stat = JSON.parse(data);
-        const time = new Date();
-        stat.push({ "operation": req.body.operation, "time": time, "product": req.body.product_name });
-        console.log(req.body);
-
-        fs.writeFile('./data/stats.json', JSON.stringify(stat), (err) => {
+app.post('/api/stats', (req, res) => {
+    fs.readFile('./server/data/stats.json', 'utf8', (err, data) => {        
+        const stat = JSON.parse(data);        
+        stat.push({...req.body, date: new Date()});                                   
+        fs.writeFile('./server/data/stats.json', JSON.stringify(stat), (err) => {
             res.end();
         });
     });
--- src/components/message-comp.js
+++
@@ -1,6 +0,0 @@
-Vue.component('message-comp', {
-    props: ['message'],
-    template: `<div>
-        <p v-if="message">нет данных</p>
-    </div>`
-})
